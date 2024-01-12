fbb738921d3194d89dc7c85009f9bbe662433363
maxie <kon1103@protonmail.com>
2024-01-12 13:08:41 +0100

    basic support for multiple subsequent archives

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/rollenreiter/delorean/parser"
 )
@@ -15,47 +16,58 @@ type cmdflags struct {
 
 func main() {
 	flags := getFlags()
-	url, err := getUrl(*flags)
-	if err != nil {
-		log.Fatal(err)
-	}
-	final := linkToArchive(*flags, url)
+	urls := getUrl(*flags)
+
+	final := linksToArchive(*flags, urls)
 
 	if flags.silentFlag {
-		fmt.Println(final)
+		for i := range final {
+			fmt.Printf("%s\n", final[i])
+		}
 	} else {
-		fmt.Printf("This is the link to the archive:\n%s\n", final)
+		fmt.Printf("These are the links to the archives:\n")
+		for i := range final {
+			fmt.Printf("%s\n", final[i])
+		}
 	}
 }
 
-func linkToArchive(f cmdflags, site string) string {
-	if !f.silentFlag {
-		fmt.Printf("Archiving %s...\n", site)
+func linksToArchive(f cmdflags, sites []string) []string {
+	archives := make([]string, len(sites))
+	for i := range sites {
+		site := sites[i]
+
+		if !f.silentFlag {
+			fmt.Printf("Archiving %s...\n", site)
+		}
+
+		url := fmt.Sprintf("https://web.archive.org/save/%s\n", site)
+		archives[i] = parser.ParseSingleLink(url)
 	}
-	url := fmt.Sprintf("https://web.archive.org/save/%s\n", site)
-	archive := parser.ParseSingleLink(url)
-	return archive
+	return archives
 }
 
-func getUrl(f cmdflags) (string, error) {
-	var err error
-	var url string
+// getting urls and saving them as a string slice of urls
+func getUrl(f cmdflags) []string {
+	u := []string{""}
 	if f.urlFlag == "" {
-		fmt.Println("Give me a link to archive:")
-		_, err := fmt.Scanln(&url)
+		fmt.Println("Enter a link to archive:")
+		_, err := fmt.Scanln(&u[0])
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		url = f.urlFlag
+		urls := f.urlFlag
+		u = strings.Fields(urls)
 	}
-	return url, err
+	return u
 }
 
+// responsible for parsing command line flags
 func getFlags() *cmdflags {
 	var f cmdflags
 	flag.BoolVar(&f.silentFlag, "s", false, "Supress all output except for the final link to the archive; useful for scripting")
-	flag.StringVar(&f.urlFlag, "u", "", "Declare a URL to archive; useful for scripting and archiving multiple URLs at once")
+	flag.StringVar(&f.urlFlag, "u", "", "Declare URLs to archive as a single string or a space-seperated sequence of strings; useful for non-interactive use, scripting and archiving multiple URLs at once")
 	flag.Parse()
 	return &f
 }
