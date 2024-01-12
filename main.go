36f7a8e6fd84224afe374bc8645cfe0903a253a3
maxie <kon1103@protonmail.com>
2024-01-12 10:06:48 +0100

    added interactive use, basic command flags

--- main.go
+++ main.go
@@ -4,36 +4,59 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/rollenreiter/delorean/parser"
 )
 
+type cmdflags struct {
+	silentFlag bool
+	urlFlag    string
+}
+
 func main() {
-	url, err := parseUrlFromCmdline()
+	flags := getFlags()
+	url, err := getUrl(*flags)
 	if err != nil {
 		log.Fatal(err)
 	}
-	final := getLinkToArchive(url)
-	fmt.Printf("This is the link to the archive:\n%s\n", final)
+	final := linkToArchive(*flags, url)
+
+	if flags.silentFlag {
+		fmt.Println(final)
+	} else {
+		fmt.Printf("This is the link to the archive:\n%s\n", final)
+	}
 }
 
-func getLinkToArchive(site string) string {
-	fmt.Printf("Archiving %s...\n", site)
+func linkToArchive(f cmdflags, site string) string {
+	if !f.silentFlag {
+		fmt.Printf("Archiving %s...\n", site)
+	}
 	url := fmt.Sprintf("https://web.archive.org/save/%s\n", site)
 	archive := parser.ParseSingleLink(url)
 	return archive
 }
 
-func parseUrlFromCmdline() (string, error) {
-	var input []string
+func getUrl(f cmdflags) (string, error) {
 	var err error
-	input = os.Args
-	flag.Parse()
-	// throw error if no url is provided
-	if input[len(input)-1] == input[0] || input[1] == "" {
-		err = fmt.Errorf("please supply a url to archive")
+	var url string
+	if f.urlFlag == "" {
+		fmt.Println("Give me a link to archive:")
+		_, err := fmt.Scanln(&url)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		url = f.urlFlag
 	}
-	url := input[len(input)-1]
 	return url, err
+}
+
+func getFlags() *cmdflags {
+	var f cmdflags
+	flag.BoolVar(&f.silentFlag, "s", false, "Supress all output except for the final link to the archive; useful for scripting")
+	flag.BoolVar(&f.silentFlag, "silent", false, "Supress all output except for the final link to the archive; useful for scripting")
+	flag.StringVar(&f.urlFlag, "u", "", "Declare a URL to archive; useful for scripting and archiving multiple URLs at once")
+	flag.Parse()
+	return &f
 }
