8d9b1d398bb985fcc06f99e303a67db7ceb0e40a
maxie <kon1103@protonmail.com>
2024-02-29 12:08:53 +0100

    made tests more readable

--- url_test.go
+++ url_test.go
@@ -10,7 +10,7 @@ func FromTestURL() cmdflags {
 	f := cmdflags{
 		urlFlag:    "https://github.com/rollenreiter/delorean junktext http://asahina.moe github.com/stompman34",
 		fileFlag:   "",
-		silentFlag: false,
+		silentFlag: true,
 		alphaFlag:  false,
 	}
 	return f
@@ -20,23 +20,21 @@ func FromTestfile() cmdflags {
 	f := cmdflags{
 		urlFlag:    "",
 		fileFlag:   "testfile",
-		silentFlag: false,
+		silentFlag: true,
 		alphaFlag:  false,
 	}
 	return f
 }
 
 func TestTokenizeUrl(t *testing.T) {
+	fmt.Println("Running TestTokenizeUrl...")
 	Flags.SetFlags(FromTestURL)
 	input := NewInput()
 	input.Tokenize()
 
-	fmt.Printf("source is the following: %s\n", input.source)
-
 	got := input.tokens
 
 	want := []string{"https://github.com/rollenreiter/delorean", "junktext", "http://asahina.moe", "github.com/stompman34"}
-	fmt.Printf("want following tokens:\n%s\n", want)
 
 	if len(got) == 0 {
 		t.Errorf("slice %q is empty, wanted %q", got, want)
@@ -46,14 +44,15 @@ func TestTokenizeUrl(t *testing.T) {
 			t.Errorf("got %q, wanted %q", got, want)
 		}
 	}
+	fmt.Println("Done.")
 }
 
 func TestTokenizeFile(t *testing.T) {
+	fmt.Println("Running TestTokenizeFile")
 	Flags.SetFlags(FromTestfile)
 	input := NewInput()
 	input.Tokenize()
 
-	fmt.Printf("source is the following: %s\n", input.source)
 	got := input.tokens
 
 	want := []string{
@@ -73,16 +72,17 @@ func TestTokenizeFile(t *testing.T) {
 			t.Errorf("got %q, wanted %q", got, want)
 		}
 	}
+	fmt.Println("Done.")
 }
 
 func TestGetUrls(t *testing.T) {
+	fmt.Println("Running TestGetUrls...")
 	Flags.SetFlags(FromTestURL)
 	var wg sync.WaitGroup
 	input := NewInput()
 	input.Tokenize()
 	input.GetUrls(&wg)
 	got := input.validUrls
-	fmt.Println(input)
 	want := []string{
 		"https://github.com/rollenreiter/delorean",
 		"http://asahina.moe",
@@ -98,17 +98,17 @@ func TestGetUrls(t *testing.T) {
 			t.Errorf("%q is not within %q", want, got)
 		}
 	}
+	fmt.Println("Done.")
 }
 
 func TestGetUrlsFile(t *testing.T) {
+	fmt.Println("Running TestGetUrlsFile...")
 	Flags.SetFlags(FromTestURL)
 	var wg sync.WaitGroup
 	input := NewInput()
 	input.Tokenize()
-	fmt.Println(input.tokens)
 	input.GetUrls(&wg)
 	got := input.validUrls
-	fmt.Println(got)
 	want := []string{
 		"https://github.com/rollenreiter/delorean",
 		"http://asahina.moe",
@@ -124,4 +124,5 @@ func TestGetUrlsFile(t *testing.T) {
 			t.Errorf("%q is not within %q", want, got)
 		}
 	}
+	fmt.Println("Done.")
 }
