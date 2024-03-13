fe4f17f4daef1b659b7add7b08a2b82f84dd612c
maxie <kon1103@protonmail.com>
2024-03-13 15:02:26 +0100

    fix: colors display properly

--- style.go
+++ style.go
@@ -1,8 +1,8 @@
 package main
 
 const (
-	Warning = "\033[43m\033[107m"
-	Error   = "\033[101m\033[107m"
-	Good    = "\033[42m\033[107m"
+	Warning = "\033[30m\033[103m"
+	Error   = "\033[39m\033[101m"
+	Good    = "\033[39m\033[42m"
 	Escape  = "\033[0m"
 )
